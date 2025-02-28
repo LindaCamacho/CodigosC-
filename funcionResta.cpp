99d8c8d11841914a594b6d65bcd2fc074b8c4cf0
LindaCamacho <154291657+LindaCamacho@users.noreply.github.com>
2025-02-28 15:23:51 -0600

    Add files via upload

---
+++ funcionResta.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+    int resta(int x, int y) {
+        return x - y; 
+    }
+
+    int main() {
+        int n1, n2; 
+
+            std::cout << "Ingrese el primer número: ";
+        std:: cin >> n1;
+
+        std::cout << "Ingrese el segundo número: ";
+        std:: cin >> n2; 
+
+        //Llamar a la función
+
+        std:: cout << "La resta es: " <<resta(n1, n2) << std::endl;
+
+        return 0; 
+    } 
