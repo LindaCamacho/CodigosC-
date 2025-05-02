3471dcced882099f8759c5c8f5b2e3a60126ab02
Linda Estrella Camacho Osorio <154291657+LindaCamacho@users.noreply.github.com>
2025-05-02 08:32:13 -0600

    Add files via upload

--- operaciones.cpp
+++ operaciones.cpp
@@ -1,11 +1,34 @@
 #include <cmath>
 #include "operaciones.h"
 
-double suma(double a, double b) {return a + b; }
-double resta(double a, double b) {return a - b; }
-double multiplicar(double a, double b) {return a * b; } 
-double divion(double a, double b) {return b != 0 ? /b : NAN; }
-double potencia(double base, double exp) {return pow(base, exp); }
-double raiz(double x) {return x >= 0 ? sqrt(x) : NAN; }
-double modulo(double a, double b) {return fmod(a, b); }
-double porcentaje(double base, double prc) {return (base * porc) / 100.0;}
+double suma(double a, double b) {
+    return a + b;
+}
+
+double resta(double a, double b) {
+    return a - b;
+}
+
+double multiplicar(double a, double b) {
+    return a * b;
+}
+
+double division(double a, double b) {
+    return b != 0 ? a / b : NAN;
+}
+
+double potencia(double base, double exp) {
+    return pow(base, exp);
+}
+
+double raiz(double x) {
+    return x >= 0 ? sqrt(x) : NAN;
+}
+
+double modulo(double a, double b) {
+    return fmod(a, b);
+}
+
+double porcentaje(double base, double prc) {
+    return (base * prc) / 100.0;
+}
